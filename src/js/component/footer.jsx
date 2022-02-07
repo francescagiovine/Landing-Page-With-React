1ad16a65cfa9bdcea440b36a5d681c5c2ee5379a
francescagiovine <francescagiovine@hotmail.com>
2022-02-07 20:43:02 +0000

    done

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+	return (
+		<nav className="navbar fixed-bottom navbar-light bg-light">
+			<a className="navbar-brand" href="#">
+				Really Cool Footer
+			</a>
+		</nav>
+	);
+};
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,17 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { Navbar } from "./navbar.jsx";
+import { Jumbotron } from "./jumbotron.jsx";
+import { Cards } from "./cardgroup.jsx";
+import { Footer } from "./footer.jsx";
 
 //create your first component
-const Home = () => {
+export const Home = () => {
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Navbar />
+			<Jumbotron />
+			<Cards />
+			<Footer />
 		</div>
 	);
 };
